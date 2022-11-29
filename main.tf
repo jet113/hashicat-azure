cf81a1d1ecc842f8366ce63c8b685bb75867246c
jet113 <47046301+jet113@users.noreply.github.com>
2022-11-29 09:56:57 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -25,6 +25,10 @@ resource "azurerm_virtual_network" "vnet" {
   location            = azurerm_resource_group.myresourcegroup.location
   address_space       = [var.address_space]
   resource_group_name = azurerm_resource_group.myresourcegroup.name
+  tags = {
+    Department = "devops",
+    Billable = true
+  }
 }
 
 resource "azurerm_subnet" "subnet" {
